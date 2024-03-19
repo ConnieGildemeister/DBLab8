cfce346dfa5798467a47b6c3e1770aacca52a148
ConnieAllure <conniegildemeister@gmail.com>
2024-03-19 13:55:19 -0700

    added code edits

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,25 @@
 const router = require('express').Router();
-const database = include('databaseConnection');
-const dbModel = include('databaseAccessLayer');
+//const database = include('databaseConnection');
+//const dbModel = include('databaseAccessLayer');
 //const dbModel = include('staticData');
+const userModel = include('models/web_user');
 
+router.get('/', async (req, res) => { 
+	console.log("page hit"); try { 
+		const users = await userModel.findAll(
+			{attributes: ['web_user_id','first_name','last_name','email']}
+			); //{where: {web_user_id: 1}} 
+			if (users === null) { 
+				res.render('error', {message: 'Error connecting to MySQL'}); 
+				console.log("Error connecting to userModel"); 
+			} else { 
+				console.log(users); res.render('index', {allUsers: users}); 
+			} } catch(ex) { 
+				res.render('error', {message: 'Error connecting to MySQL'}
+				); 
+				console.log("Error connecting to MySQL"); console.log(ex); 
+			} }); 
+/*
 router.get('/', async (req, res) => {
 	console.log("page hit");
 	
@@ -60,6 +77,6 @@ router.get('/deleteUser', async (req, res) => {
 		}
 	}
 });
-
+*/
 
 module.exports = router;
